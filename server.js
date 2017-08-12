3b6a9fdd6b06bb0078d408fa7faabdff40615f75
a-vinutha <30804506+a-vinutha@users.noreply.github.com>
2017-08-12 15:21:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,20 +5,91 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : 'Article One',
+    heading : 'Article-One',
+    content : `<div><p>Here goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article one
+            </p>
+        </div>
+        <div>
+            <p>Here goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article one
+            </p>
+        </div>
+        <div>
+            <p>Here goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article oneHere goes the contents for Article one
+            </p></div>`
+}
+
+var articleTwo = {
+    title : 'Article Two',
+    heading : 'Article-Two',
+    content : `<div><p>Here goes the contents for Article Two.Here goes the contents for Article Two.Here goes the contents for Article Two.Here goes the contents for Article Two.Here goes the contents for Article Two.
+            </p>
+        </div>
+        <div><p>Here goes the contents for Article Two.Here goes the contents for Article Two.Here goes the contents for Article Two.Here goes the contents for Article Two.Here goes the contents for Article Two.
+            </p>
+        </div><div><p>Here goes the contents for Article Two.Here goes the contents for Article Two.Here goes the contents for Article Two.Here goes the contents for Article Two.Here goes the contents for Article Two.
+            </p>
+        </div>`
+}
+
+var articleThree = {
+    title : 'Article Three',
+    heading : 'Article-Three',
+    content : `<div><p>Here goes the contents for Article Three.Here goes the contents for Article Three.Here goes the contents for Article Three.Here goes the contents for Article Three.Here goes the contents for Article Three.
+            </p>
+        </div>
+        <div><p>Here goes the contents for Article Three.Here goes the contents for Article Three.Here goes the contents for Article Three.Here goes the contents for Article Three.Here goes the contents for Article Three.
+            </p>
+        </div><div><p>Here goes the contents for Article Three.Here goes the contents for Article Three.Here goes the contents for Article Three.Here goes the contents for Article Three.Here goes the contents for Article Three.
+            </p>
+        </div>`
+}
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `<html>
+    <head>
+        <title>
+            $(title)
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>$(heading)</h3>
+        $(content)
+        </div>
+    </body>
+</html>`;
+
+return htmlTemplate;    
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send(createTemplate(articleTwo));
 });
 
 app.get('/article-three', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.send(createTemplate(articleThree));
 });
 
 app.get('/ui/style.css', function (req, res) {
