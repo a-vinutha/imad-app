dd7eb542abb35c9b5d3f6b88d55b5347fc0c000f
a-vinutha <30804506+a-vinutha@users.noreply.github.com>
2017-08-13 14:57:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var img = document.getElementById('image');
+function moveRight(){
+    marginLeft = marginLeft+5;
+}
+
+img.onclick = function(){
+    var interval = setInterval(moveRight, 50);
+};
