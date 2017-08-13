ed3baa389a47578613552d470e222622e15f347f
a-vinutha <30804506+a-vinutha@users.noreply.github.com>
2017-08-13 14:58:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,11 @@
 console.log('Loaded!');
 
 var img = document.getElementById('image');
+
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft+5;
+    img.stylr.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function(){
