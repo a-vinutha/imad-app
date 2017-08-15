8fde213626a76965a0cd4be855cc163322c4fba0
a-vinutha <30804506+a-vinutha@users.noreply.github.com>
2017-08-15 19:27:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,6 @@ button.onclick = function(){
           if(request.status == 200){
               var counter = request.responseText;
               var span = document.getElementById('count');
-              counter = counter + 1;
               span.innerHTML = counter.toString();
           }
       }
