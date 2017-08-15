413e9f0c9882eb133e112a333d95985dee0db737
a-vinutha <30804506+a-vinutha@users.noreply.github.com>
2017-08-15 19:04:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,9 @@
-console.log('Loaded!');
-
-var img = document.getElementById('image');
-
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft+5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+var button = document.getElementById('counter');
+button.onClick =function(){
+    
+    
+    counter =counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
 };
