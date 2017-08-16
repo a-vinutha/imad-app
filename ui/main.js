412049c2de91dd8b774e11deac57e074eef862f6
a-vinutha <30804506+a-vinutha@users.noreply.github.com>
2017-08-16 20:36:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,6 @@ submit.onclick = function(){
     for(var i =0 ; i< names.length;i++){
         list += '<li>' + names[i] + '</li>';
     }
-    var ul = document.getElementById('nameslist');
+    var ul = document.getElementById('namelist');
     ul.innerHtml = list;
-    
 };
