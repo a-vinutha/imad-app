6653f3585a01e034e3f73abfcba6f8f0fae549aa
a-vinutha <30804506+a-vinutha@users.noreply.github.com>
2017-08-16 20:54:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ var names=[];
 app.get('/submit-name/:name',function(req,res){
    var name = req.params.name;
    names.push(name);
-   res.send(names);
+   res.send(JSON.stringify(names));
 });
 
 app.get('/:articleName', function(req,res){
