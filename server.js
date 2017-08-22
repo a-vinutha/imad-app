99d3f569bf8da87b5a147aa2e73559f7358561db
a-vinutha <30804506+a-vinutha@users.noreply.github.com>
2017-08-22 21:13:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,6 @@ app.post('/create-user', function(req, res) {
 });
 
 app.post('/login', function(req, res){
-    
     var username = req.body.username;
     var password = req.body.password;
     
