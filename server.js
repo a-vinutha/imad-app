f050d2491041c9bdecf99dc169225b46c97d7c74
a-vinutha <30804506+a-vinutha@users.noreply.github.com>
2017-08-22 21:09:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,35 @@ app.post('/create-user', function(req, res) {
     });
 });
 
+app.post('/login', function(req, res){
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    pool.query('SELECT * from  "user" username = $1', [username], function (err,result){
+        if (err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length === 0)   {
+                 res.status(404).send('username/password invalid');
+            }
+                 else{
+                 var dbString = result.rows[0].password;
+                 var salt = dbString.split('$')[2];
+                 var hashedPassword = hash(password, salt);
+                 if(hashedPassword === dbString){
+                    res.send('Credentials Correct');
+                 }
+                 else{
+                     res.send(403).send('username/password is invalid');
+                 }
+             }
+         }
+
+    });
+    
+});
+
 app.get('/articles/:articleName', function(req,res){
     var articleName = req.params.articleName;
     
